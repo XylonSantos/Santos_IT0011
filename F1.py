41e80af9637916452d7e9b1fb19e0b8a6bd3f685
XylonSantos <202311992@fit.edu.ph>
2025-01-23 11:59:47 +0800

    update

---
+++ F1.py
@@ -0,0 +1,21 @@
+FirstName = input ("Enter First Name: ")
+LastName = input ("Enter Last Name: ")
+MiddleName = input ("Enter Middle Name: ")
+Birthday = input ("Enter Birthday: ")
+Gender = input ("Enter Gender: ")
+Address = input ("Enter Address: ")
+Course = input ("Enter Course: ")
+YearLevel = input ("Year Level: ")
+print()
+print ("First Name:", FirstName)
+print ("Last Name:", LastName)
+print ("Middle Name:", MiddleName)
+print ("Birthday:", Birthday)
+print ("Gender:", Gender)
+print ("Address:", Address)
+print ("Course:", Course)
+print ("Year Level:", YearLevel)
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,2 @@
+name = "Xylon"
+print("Hello," + name + "!")
